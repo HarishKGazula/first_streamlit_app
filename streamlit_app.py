b854fecdb7fff2a7655c71baa92193738d8dd13a
HarishKGazula <152335830+HarishKGazula@users.noreply.github.com>
2023-11-30 05:53:24 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+	  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -32,13 +38,28 @@ try:
   if not fruit_choice:
       streamlit.error("please select a fruit to get information.")
   else:  
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      strealit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
 #dont run anything past here
+streamlit.header("The fruit load lost contains:")
+#Snowflake-related functions
+    def get_fruit_load_list():
+    	with my_cnx.cursor() as my_cur:
+    		 my_cur.execute("select * from fruit_load_list")
+    		 return my_cur.fetchall()
+
+# Add a button to load the fruit
+    if streamlit.button('Get Fruit Load List'):		 
+    	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    	my_data_rows = get_fruit_load_list()
+    	streamlit.dataframe(my_data_rows)
+
+
+
+
 streamlit.stop()
 
 #import snowflake.connector
